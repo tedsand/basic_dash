c42739ff08f9978293d940472be622107c89a58a
Ted Sand <tedhsand@gmail.com>
2020-04-27 18:16:43 -0400

    added callback graph

---
+++ pages/tutorial_callback_graph.py
@@ -0,0 +1,55 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from utils import Header
+from app import app
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+layout = html.Div([
+    html.Div([Header(app)]),
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='year-slider',
+        min=df['year'].min(),
+        max=df['year'].max(),
+        value=df['year'].min(),
+        marks={str(year) : str(year) for year in df['year'].unique()},
+        step=None
+    )
+])
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+    traces = []
+    for i in filtered_df.continent.unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == i]
+        traces.append(dict(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            text=df_by_continent['country'],
+            mode = 'markers',
+            opacity = 0.7,
+            marker = {
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name = i
+        ))
+
+    return {
+        'data':traces,
+        'layout':dict(
+            xaxis={'type':'log','title': 'GDP Per Capita',
+                   'range':[2.3,4.8]},
+            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest',
+            transition={'duration': 500},
+        )
+    }
